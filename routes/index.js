4431f06dc6d34f9f537532232bafd92bf3ae7f81
Boyko Anton <boyko.ant@live.com>
2015-04-24 18:15:36 +0300

    registration closed

--- routes/index.js
+++ routes/index.js
@@ -17,69 +17,76 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-  function isEmpty(val) {
-      return typeof(val) === 'undefined'
-          || val === null
-          || val.length === 0;
-  }
+    res.render('index', {
+        partials: getPartials(),
+        isShowRegistrationForm: true
+    }, function(err, html) {
+        res.send(getMinifiedHtml(html));
+    });
 
-  var firstName = req.body.tbFirstName;
-  var lastName = req.body.tbLastName;
-  var email = req.body.tbEmail;
-
-  if (isEmpty(firstName) || isEmpty(lastName) || isEmpty(email)) {
-      res.render('index', {
-          partials: getPartials(),
-          isShowRegistrationForm: true,
-          errorMessage: 'Все поля обязательны для заполнения.',
-          firstName: firstName,
-          lastName: lastName,
-          email: email
-      }, function(err, html) {
-          res.send(getMinifiedHtml(html));
-      });
-
-    return;
-  }
-
-  var entity = {
-      PartitionKey: { '_' : '2015' },
-      RowKey: { '_' : email },
-      FirstName: { '_' : firstName },
-      LastName: { '_' : lastName }
-  };
-
-  dataStorage.insertEntity('GWABRegistration', entity).then(function(result){
-      if (result.isError) {
-          var errorMessage = result.errorMessage || 'Простите, произошла ошибка. Пожалуйста, попробуйте пройти регистрацию повторно.';
-
-          res.render('index', {
-              partials: getPartials(),
-              isShowRegistrationForm: true,
-              errorMessage: errorMessage
-          }, function(err, html) {
-              res.send(getMinifiedHtml(html));
-          });
-
-          return;
-      }
-      var entityFeedback = {
-          PartitionKey: { '_' : '2015' },
-          RowKey: { '_' : uuid.v4() },
-          EMail: { '_' : email },
-          FirstName: { '_' : firstName },
-          LastName: { '_' : lastName }
-      };
-
-      dataStorage.insertEntity('GWABFeedback', entityFeedback).then(function(){
-          res.render('index', {
-              partials: getPartials(),
-              isShowRegistrationForm: false
-          }, function(err, html) {
-              res.send(getMinifiedHtml(html));
-          });
-      });
-  });
+  //function isEmpty(val) {
+  //    return typeof(val) === 'undefined'
+  //        || val === null
+  //        || val.length === 0;
+  //}
+  //
+  //var firstName = req.body.tbFirstName;
+  //var lastName = req.body.tbLastName;
+  //var email = req.body.tbEmail;
+  //
+  //if (isEmpty(firstName) || isEmpty(lastName) || isEmpty(email)) {
+  //    res.render('index', {
+  //        partials: getPartials(),
+  //        isShowRegistrationForm: true,
+  //        errorMessage: 'Все поля обязательны для заполнения.',
+  //        firstName: firstName,
+  //        lastName: lastName,
+  //        email: email
+  //    }, function(err, html) {
+  //        res.send(getMinifiedHtml(html));
+  //    });
+  //
+  //  return;
+  //}
+  //
+  //var entity = {
+  //    PartitionKey: { '_' : '2015' },
+  //    RowKey: { '_' : email },
+  //    FirstName: { '_' : firstName },
+  //    LastName: { '_' : lastName }
+  //};
+  //
+  //dataStorage.insertEntity('GWABRegistration', entity).then(function(result){
+  //    if (result.isError) {
+  //        var errorMessage = result.errorMessage || 'Простите, произошла ошибка. Пожалуйста, попробуйте пройти регистрацию повторно.';
+  //
+  //        res.render('index', {
+  //            partials: getPartials(),
+  //            isShowRegistrationForm: true,
+  //            errorMessage: errorMessage
+  //        }, function(err, html) {
+  //            res.send(getMinifiedHtml(html));
+  //        });
+  //
+  //        return;
+  //    }
+  //    var entityFeedback = {
+  //        PartitionKey: { '_' : '2015' },
+  //        RowKey: { '_' : uuid.v4() },
+  //        EMail: { '_' : email },
+  //        FirstName: { '_' : firstName },
+  //        LastName: { '_' : lastName }
+  //    };
+  //
+  //    dataStorage.insertEntity('GWABFeedback', entityFeedback).then(function(){
+  //        res.render('index', {
+  //            partials: getPartials(),
+  //            isShowRegistrationForm: false
+  //        }, function(err, html) {
+  //            res.send(getMinifiedHtml(html));
+  //        });
+  //    });
+  //});
 });
 
 function getPartials() {
